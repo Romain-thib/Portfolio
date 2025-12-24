c3cb0bcea74a84f951bf77bb38cbcd3f78b370c1
romain <thibautromain7@gmail.com>
2025-12-24 14:26:33 +0100

    utiliser la version avec les tuiles pour la page about

--- src/components/about/Timeline.vue
+++ src/components/about/Timeline.vue
@@ -8,14 +8,14 @@ const timelineItems = ['but', 'lycee']
 </script>
 
 <template>
-  <div class="w-full mt-15">
+  <div class="w-full mt-1 -ml-10">
     <h2 class="relative z-10 text-4xl font-semibold tracking-tight mb-10 text-zinc-800 dark:text-amber-400 text-center">
       {{ t('timeline.title') }}
     </h2>
 
-    <div class="relative flex flex-col gap-8">
+    <div class="relative flex flex-col gap-8 pl-6 ml-[20%]">
       <div class="absolute -left-16 top-0 bottom-0 w-1 bg-gray-300 dark:bg-slate-800 rounded">
-      <div class="absolute left-1/2 -translate-x-1/2 w-5 h-5 border-l-5 border-t-5 border-gray-300 dark:border-slate-800 rotate-45 bg-white dark:bg-slate-900"></div>
+      <div class="absolute top- left-1/2 -translate-x-1/2 w-5 h-5 border-l-5 border-t-5 border-gray-300 dark:border-slate-800 rotate-45 bg-white dark:bg-slate-900"></div>
       </div>
       
       <TimelineItem
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -5,7 +5,7 @@
 
 <template>
   <div class="min-h-screen pt-15 pb-12 mb-10">
-    <div class=" mx-auto">
+    <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-12 max-w-15xl mx-auto">
 
       <div class="mx-10 mt-1 flex flex-col items-center justify-start">
         <h2 class="text-4xl font-semibold tracking-tight mb-10 text-zinc-800 dark:text-amber-400 text-center">
@@ -13,7 +13,7 @@
         </h2>
 
         <p class="text-justify leading-relaxed text-lg text-slate-700 dark:text-slate-300 
-                  w-full max-w-3xl p-10 bg-stone-200 dark:bg-slate-900
+                  w-full max-w-xl p-10 bg-stone-200 dark:bg-slate-900
                   border border-stone-300 dark:border-slate-800 rounded-xl">
           {{ $t("presentation.description1") }}
           <br><br>
@@ -23,10 +23,22 @@
         </p>
       </div>
 
-      <div class=" mt-20 lg:mt-0">
+      <div class="mx-10 mt-15 flex flex-col items-center justify-start 2xl:ml-[0%] mr-10 h-200 lg:mt-0 lg:border-l lg:border-slate-300 dark:lg:border-amber-400 lg:pl-12">
         <Timeline>
+          <TimelineItem
+            :title="$t('timeline.but.title')"
+            :subtitle="$t('timeline.but.subtitle')"
+            :description="$t('timeline.but.description')"
+          />
+
+          <TimelineItem
+            :title="$t('timeline.lycee.title')"
+            :subtitle="$t('timeline.lycee.subtitle')"
+            :description="$t('timeline.lycee.description')"
+          />
         </Timeline>
       </div>
+      
     </div>
   </div>
 </template>
