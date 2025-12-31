79ae8e4a1a797f494e66c6908378dbcdf969081f
romain <thibautromain7@gmail.com>
2025-12-31 20:01:41 +0100

    Push sur gh-pages

--- src/components/LanguageSwitcher.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import { useUiStore } from '../store/ui'
-const ui = useUiStore()
-</script>
-
-<template>
-  <button @click="ui.setLanguage('fr')">FR</button>
-  <button @click="ui.setLanguage('en')">EN</button>
-</template>
